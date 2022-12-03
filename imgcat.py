1f48eebede4af8b06f5e1299a149affdbd4f9dd1
Noa Moulin <67688797+Cogema235@users.noreply.github.com>
2022-12-03 01:43:07 +0100

    Update imgcat.py

--- imgcat.py
+++ imgcat.py
@@ -14,14 +14,11 @@ class Plan:
         abss = list(map(lambda elm : elm.coords[0], planches))
         ords = list(map(lambda elm : elm.coords[1], planches))
         image_at = lambda coords, planches, void : (list(map(lambda elm : elm.image, filter(lambda elm : elm.coords == coords, planches))) + [void])[0]
-        matrix = [[image_at((x, y), planches, cv2.resize(void, dsize = (0,0), fx = SIZE, fy = SIZE))
-            for x in range(min(abss), max(abss) + 1)]
-            for y in range(min(ords), max(ords) + 1)]
+        matrix = [[image_at((x, y), planches, cv2.resize(void, dsize = (0,0), fx = SIZE, fy = SIZE)) for x in range(min(abss), max(abss) + 1)] for y in range(min(ords), max(ords) + 1)]
         self.image = cv2.vconcat(list(map(lambda row : cv2.hconcat(row), matrix)))
 
-plan = Plan(list(map(lambda path : Planche(path),
-    list(filter(lambda elm : not elm in (os.path.realpath(__file__).split('/')[-1], BACKGROUND_PATH), os.listdir())))), cv2.imread(BACKGROUND_PATH))
+plan = Plan(list(map(lambda path : Planche(path), list(filter(lambda elm : not elm in (os.path.realpath(__file__).split('/')[-1], BACKGROUND_PATH), os.listdir())))), cv2.imread(BACKGROUND_PATH))
 
 cv2.imwrite(OUTPUT_FILE, plan.image)
 
-#un peu illisible, mais élégant ! (si)
+#un peu illisible
